7615b823dcdd538f5b3f0bd78700af482fcf6f78
Olga <olgabel_96@mail.ru>
2017-11-21 16:05:51 +0300

    Добавила main

--- lab1/src/main/java/ru/vsu/lab1/Main.java
+++ lab1/src/main/java/ru/vsu/lab1/Main.java
@@ -1,7 +1,15 @@
 package ru.vsu.lab1;
 
+
+
+/**
+ * Я здесь главная
+ */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World!");
+        LocalDate a= new LocalDate(2004,12,25);
+        int b=a.getDayOfMonth();
+        System.out.println(b);
     }
 }
